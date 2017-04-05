9e32f4ceede3a2cbef4e99e20adf1729009b543f
Josh Fongheiser <josh.fongsoft@gmail.com>
2017-04-05 19:55:25 -0400

    Add addon shell

---
+++ PlayedAll.lua
@@ -0,0 +1,17 @@
+-- Create a namespace.
+PlayedAll = {}
+
+PlayedAll.name = "PlayedAll"
+
+function PlayedAll:Initialize()
+
+end
+
+-- When addons are loaded, run our plugin's init function.
+function PlayedAll.OnAddOnLoaded(event, addonName)
+	if addonName = PlayedAll.name then
+		PlayedAll:Initialize()
+	end
+end
+
+EVENT_MANAGER:RegisterForEvent(PlayedAll.name, EVENT_ADD_ON_LOADED, PlayedAll.OnAddOnLoaded)
