520c24b28c66284433a107ccdf44ee30015a667a
Josh Fongheiser <josh.fongsoft@gmail.com>
2017-04-05 20:03:51 -0400

    Fix incorrect If syntax

--- PlayedAll.lua
+++ PlayedAll.lua
@@ -3,13 +3,14 @@ PlayedAll = {}
 
 PlayedAll.name = "PlayedAll"
 
+-- Addon's Init function.
 function PlayedAll:Initialize()
 
 end
 
 -- When addons are loaded, run our plugin's init function.
 function PlayedAll.OnAddOnLoaded(event, addonName)
-	if addonName = PlayedAll.name then
+	if addonName == PlayedAll.name then
 		PlayedAll:Initialize()
 	end
 end
